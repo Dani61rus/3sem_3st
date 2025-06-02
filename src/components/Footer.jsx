ea40847a39a4da9471195f6b6aadd63473d85d63
Dani61rus <162551400+Dani61rus@users.noreply.github.com>
2025-06-02 23:19:27 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
 export function Footer(){
     return (
-        <footer className="bg-red-500 text-white py-4 mt-10">
+        <footer className="bg-purple-400 text-white py-4 mt-10">
             <div className="container mx-auto px4 flex justify-between items-center text-sm">
                 <span>© {new Date().getFullYear()}</span>
                 <a
-                    href="https://github.com/p3mg"
+                    href="https://github.com/Dani61rus/3sem_3st"
                     className="text-gray-200 hover:text-white transition-colors duration-200"
                 >
                 Repository
